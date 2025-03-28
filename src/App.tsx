f5bf6494e9adbdb68baf41d7d3a9bb052c383b48
Tom Whitton <tom@piertwo.com>
2025-03-28 13:25:52 +1000

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ValidatorOptimizer } from "./components/ValidatorOptimizer";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <ValidatorOptimizer />
+    </div>
+  );
+};
+
+export default App;
